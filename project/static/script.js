159aea0d47f3c9bef30a0d4368cf63b35d72c85c
Aleksander Pugowski <aleksanderpugowski@gmail.com>
2024-05-18 13:00:53 +0200

    Add coloring table cells based on score

--- project/static/script.js
+++ project/static/script.js
@@ -1,3 +1,4 @@
+// Search on table
 $(document).ready(function() {
     var $rows = $('#mainTable tr:gt(0)');
     console.log($rows)
@@ -8,6 +9,24 @@ $(document).ready(function() {
             var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
             return !~text.indexOf(val);
         }).hide();
+    });
+});
+
+// Color table cells
+$(document).ready(function () {
+
+function getColor(value){
+// https://stackoverflow.com/questions/7128675/from-green-to-red-color-depend-on-percentage
+    //value from 0 to 1
+    const MAX_VALUE = 5.0
+    value = value * 1.0 / MAX_VALUE
+    var hue=(value*120).toString(10);
+    return ["hsl(",hue,",100%,50%)"].join("");
+}
+    $(".mainTableCell").each(function () {
+        const value = parseInt($(this).text());
+        const color = getColor(value);
+        $(this).css("background-color", color);
     });
 });
 
